41fe5957b455d6ca6941741b60a2114343f8b841
San-Goon <tigerlove95@naver.com>
2021-10-12 18:47:55 +0900

    211012

---
+++ schemas/post.js
@@ -0,0 +1,81 @@
+export default {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "blockContent",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "createdAt",
+      title: "Created at",
+      type: "datetime",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "thumbnail",
+      title: "Thumbnail",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "alt",
+          options: {
+            isHighlighted: true,
+          },
+        },
+      ],
+    },
+    {
+      name: "tag",
+      title: "Tag",
+      type: "reference",
+      to: { type: "tag" },
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "thumbnail",
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      });
+    },
+  },
+};
